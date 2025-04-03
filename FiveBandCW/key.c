cae63e769915c96f86420e1d00e3f2219a75a7bf
GoodEarthWeather <fishineer@gmail.com>
2025-04-02 19:20:38 -0700

    iambic mode correction
    iambic a, b and ultimatic seem to work now.

--- FiveBandCW/key.c
+++ FiveBandCW/key.c
@@ -246,17 +246,16 @@ void ditdah(uint8_t key)
     uint8_t count;
     uint8_t ditKeyState;
     uint8_t dahKeyState;
+    uint8_t squeeze = DISABLED;
     extern uint8_t txMode;
     extern uint8_t paddleOrientation;  // paddles configured for dah-dit or dit-dah
     extern uint8_t cwMsgState;  // indicates if recording cw message
     extern uint16_t *cwMemPtr;
     extern uint8_t volatile buttonPressed;
 
-
-    //  Determine state of dit and dah keys
     do {
 
-        // Now start the sidetone and, if needed, cw message timer
+        // start the sidetone and, if needed, cw message timer
         Timer_A_startCounter(TIMER_A0_BASE,TIMER_A_UP_MODE);  // start side tone
         if (cwMsgState == RECORD)
         {
@@ -322,18 +321,24 @@ void ditdah(uint8_t key)
         }
 
         if (ditKeyState == GPIO_INPUT_PIN_LOW && dahKeyState == GPIO_INPUT_PIN_LOW){
+            squeeze = ENABLED;
             if (iambicMode != IAMBIC_MODE_ULTIMATIC){
                 (key == DAH) ? (key = DIT) : (key = DAH);  // alternate dit/dah
             } else if (buttonPressed != BTN_PRESSED_NONE){
                 (buttonPressed == BTN_PRESSED_DIT) ? (key = DIT) : (key = DAH);
             }
         } else if (ditKeyState == GPIO_INPUT_PIN_LOW && dahKeyState == GPIO_INPUT_PIN_HIGH) {
+            squeeze = DISABLED;
             key = DIT;
         } else if (ditKeyState == GPIO_INPUT_PIN_HIGH && dahKeyState == GPIO_INPUT_PIN_LOW) {
+            squeeze = DISABLED;
             key = DAH;
         } else if (ditKeyState == GPIO_INPUT_PIN_HIGH && dahKeyState == GPIO_INPUT_PIN_HIGH) {
-            if (iambicMode != IAMBIC_MODE_A) {
-                if (buttonPressed == BTN_PRESSED_DIT) {
+            if (iambicMode == IAMBIC_MODE_B) {
+                if (squeeze == ENABLED) {
+                    squeeze = DISABLED;
+                    (key == DAH) ? (key = DIT) : (key = DAH);
+                } else if (buttonPressed == BTN_PRESSED_DIT) {
                     key = DIT;
                 } else if (buttonPressed == BTN_PRESSED_DAH) {
                     key = DAH;
@@ -349,251 +354,3 @@ void ditdah(uint8_t key)
     buttonPressed = BTN_PRESSED_NONE;
 }
 
-
-
-
-
-/**********************************88
-// get state of keys
-ditKeyState = GPIO_getInputPinValue(ditKey);
-dahKeyState = GPIO_getInputPinValue(dahKey);
-
-// iambic test
-if ( (dahKeyState == GPIO_INPUT_PIN_LOW) && (ditKeyState == GPIO_INPUT_PIN_LOW) )  // keys are squeezed
-{
-    (key == DIT) ? (key = DAH) : (key = DIT);  // alternate
-} else {
-    (dahKeyState == GPIO_INPUT_PIN_LOW) ? (key = DAH) : (key = DIT);
-}
-if ((ditKeyState == GPIO_INPUT_PIN_HIGH) && (dahKeyState == GPIO_INPUT_PIN_HIGH))
-    break;
-
-// check state of dit/dah keys - if one of keys is pressed, loop back and repeat
-ditKeyState = GPIO_getInputPinValue(ditKey);
-dahKeyState = GPIO_getInputPinValue(dahKey);
-
-if ((key == DIT) && (ditKeyState == GPIO_INPUT_PIN_HIGH))
-    break;
-if ((key == DAH) && (dahKeyState == GPIO_INPUT_PIN_HIGH))
-    break;
-******************************/
-
-// original ditdah() routine
-/********************************************************************************
-// This routine is to handle the dit and dah key
-// 'key' is either DIT or DAH (i.e. 1 or 3)
-void ditdah(uint8_t key)
-{
-    uint8_t done;
-    uint8_t count;
-    uint8_t ditKeyState;
-    uint8_t dahKeyState;
-    extern uint8_t txMode;
-    extern uint8_t paddleOrientation;  // paddles configured for dah-dit or dit-dah
-    extern uint8_t cwMsgState;  // indicates if recording cw message
-    extern uint16_t *cwMemPtr;
-
-    iambicMode = DISABLED;
-
-    //  Determine state of dit and dah keys
-    do {
-        if (paddleOrientation == PADDLE_DAH_DIT){
-            ditKeyState = GPIO_getInputPinValue(DIT_KEY);
-            dahKeyState = GPIO_getInputPinValue(DAH_KEY);
-        }
-        else {
-            ditKeyState = GPIO_getInputPinValue(DAH_KEY);
-            dahKeyState = GPIO_getInputPinValue(DIT_KEY);
-        }
-        // iambic test
-        if ( (dahKeyState == GPIO_INPUT_PIN_LOW) && (ditKeyState == GPIO_INPUT_PIN_LOW) )  // iambic mode, so alternate dit/dah
-        {
-            (key == DIT) ? (key = DAH) : (key = DIT);
-            iambicMode = ENABLED;
-        } else {
-            iambicMode = DISABLED;
-            // not in iambic mode, so set key to whatever key remains pressed
-            (dahKeyState == GPIO_INPUT_PIN_LOW) ? (key = DAH) : (key = DIT);
-            // end of iambic test
-        }
-        if ((ditKeyState == GPIO_INPUT_PIN_HIGH) && (dahKeyState == GPIO_INPUT_PIN_HIGH))
-            break;
-
-        // Now start the sidetone and, if needed, cw message timer
-        Timer_A_startCounter(TIMER_A0_BASE,TIMER_A_UP_MODE);  // start side tone
-        if (cwMsgState == RECORD)
-        {
-            Timer_A_stop(TIMER_A3_BASE);  // stop cw msg timer
-            if (Timer_A_getInterruptStatus(TIMER_A3_BASE) == TIMER_A_INTERRUPT_PENDING)
-            {
-                // timer overflow - limit delay to max (2 seconds)
-                *cwMemPtr++ = 0xFFFF;
-                Timer_A_clearTimerInterrupt(TIMER_A3_BASE);
-            } else {
-                *cwMemPtr++ = Timer_A_getCounterValue(TIMER_A3_BASE);
-            }
-            Timer_A_clear(TIMER_A3_BASE);  // clear timer
-            Timer_A_startCounter(TIMER_A3_BASE,TIMER_A_CONTINUOUS_MODE);  // start measuring keyDown time
-        }
-
-        // if transmitter enabled, turn on
-        if (txMode == ENABLED)
-            keyDown();
-
-        // Prepare dit dah timer
-        Timer_A_clearCaptureCompareInterrupt(TIMER_A2_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_0);
-        Timer_A_clear(TIMER_A2_BASE);  // clear timer
-
-        // Start dit/dah delay timer
-        count = 0;
-        while (count < key)
-        {
-            done = Timer_A_getCaptureCompareInterruptStatus(TIMER_A2_BASE,TIMER_A_CAPTURECOMPARE_REGISTER_0,TIMER_A_CAPTURECOMPARE_INTERRUPT_FLAG);
-            if (done == TIMER_A_CAPTURECOMPARE_INTERRUPT_FLAG)
-            {
-                count++;
-                Timer_A_clearCaptureCompareInterrupt(TIMER_A2_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_0);
-            }
-        }
-
-        //  stop transmission (if enabled) and stop side tone and cw message timer if needed
-        if (txMode == ENABLED)
-            keyUp();
-        Timer_A_stop(TIMER_A0_BASE);  // stop side tone
-        if (cwMsgState == RECORD)
-        {
-            Timer_A_stop(TIMER_A3_BASE);  // stop cw msg timer
-            *cwMemPtr++ = Timer_A_getCounterValue(TIMER_A3_BASE);
-            Timer_A_clear(TIMER_A3_BASE);  // clear timer
-            Timer_A_startCounter(TIMER_A3_BASE,TIMER_A_CONTINUOUS_MODE);  // start measuring keyUp time
-        }
-
-        // Wait one dit time
-        Timer_A_clear(TIMER_A2_BASE);  // clear dit/dah timer
-        Timer_A_clearCaptureCompareInterrupt(TIMER_A2_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_0);
-        do {
-            done = Timer_A_getCaptureCompareInterruptStatus(TIMER_A2_BASE,TIMER_A_CAPTURECOMPARE_REGISTER_0,TIMER_A_CAPTURECOMPARE_INTERRUPT_FLAG);
-        } while (done != TIMER_A_CAPTURECOMPARE_INTERRUPT_FLAG);
-        Timer_A_clearCaptureCompareInterrupt(TIMER_A2_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_0);
-
-        // check state of dit/dah keys - if one of keys is pressed, loop back and repeat
-        if (paddleOrientation == PADDLE_DAH_DIT){
-            ditKeyState = GPIO_getInputPinValue(DIT_KEY);
-            dahKeyState = GPIO_getInputPinValue(DAH_KEY);
-        }
-        else {
-            ditKeyState = GPIO_getInputPinValue(DAH_KEY);
-            dahKeyState = GPIO_getInputPinValue(DIT_KEY);
-        }
-        if ((key == DIT) && (ditKeyState == GPIO_INPUT_PIN_HIGH))
-            break;
-        if ((key == DAH) && (dahKeyState == GPIO_INPUT_PIN_HIGH))
-            break;
-
-    } while (((dahKeyState) == GPIO_INPUT_PIN_LOW) || (ditKeyState == GPIO_INPUT_PIN_LOW));
-
-}
-******************/
-/********************************
-if (iambicMode == IAMBIC_MODE_B)
-{
-    if (key == DAH)  // dah key was sent
-    {
-        if (dahKeyState == GPIO_INPUT_PIN_LOW)  // dah key still pressed
-        {
-            if (buttonPressed == BTN_PRESSED_DIT || ditKeyState == GPIO_INPUT_PIN_LOW)
-            {
-                buttonPressed = BTN_PRESSED_NONE;
-                key = DIT;
-            } else {
-                buttonPressed = BTN_PRESSED_NONE;
-                key = DAH;
-            }
-        } else if (buttonPressed == BTN_PRESSED_DIT || ditKeyState == GPIO_INPUT_PIN_LOW) {  // dah key not pressed, but dit key was pressed
-            buttonPressed = BTN_PRESSED_NONE;
-            key = DIT;
-        }
-        else
-            break;
-    } else {  // dit key was sent
-        if (ditKeyState == GPIO_INPUT_PIN_LOW)  // dit key still pressed
-        {
-            if (buttonPressed == BTN_PRESSED_DAH || dahKeyState == GPIO_INPUT_PIN_LOW)
-            {
-                buttonPressed = BTN_PRESSED_NONE;
-                key = DAH;
-            } else {
-                buttonPressed = BTN_PRESSED_NONE;
-                key = DIT;
-            }
-        } else if (buttonPressed == BTN_PRESSED_DAH || dahKeyState == GPIO_INPUT_PIN_LOW) {  // dit key not pressed, but dah key was pressed
-            buttonPressed = BTN_PRESSED_NONE;
-            key = DAH;
-        }
-        else
-            break;
-    }
-}
-else if (iambicMode == IAMBIC_MODE_A)
-{
-    if (key == DAH)  // dah key was sent
-    {
-        if (dahKeyState == GPIO_INPUT_PIN_LOW)  // dah key still pressed
-        {
-            (ditKeyState == GPIO_INPUT_PIN_LOW) ? (key = DIT) : (key = DAH);
-        } else if (ditKeyState == GPIO_INPUT_PIN_LOW) {  // dah key not pressed, but dit key was pressed
-            key = DIT;
-        }
-        else
-            break;
-    } else {  // dit key was sent
-        if (ditKeyState == GPIO_INPUT_PIN_LOW)  // dit key still pressed
-        {
-            (dahKeyState == GPIO_INPUT_PIN_LOW) ? (key = DAH) : (key = DIT);
-        } else if (dahKeyState == GPIO_INPUT_PIN_LOW) {  // dit key not pressed, but dah key was pressed
-            key = DAH;
-        }
-        else
-            break;
-    }
-}
-else if (iambicMode == IAMBIC_MODE_ULTIMATIC)
-{
-    if (key == DAH)  // dah key was sent
-    {
-        if (dahKeyState == GPIO_INPUT_PIN_LOW)  // dah key still pressed
-        {
-            if (buttonPressed == BTN_PRESSED_DIT || ditKeyState == GPIO_INPUT_PIN_LOW)
-            {
-                buttonPressed = BTN_PRESSED_NONE;
-                key = DAH;
-            } else {
-                buttonPressed = BTN_PRESSED_NONE;
-                key = DAH;
-            }
-        } else if (buttonPressed == BTN_PRESSED_DIT || ditKeyState == GPIO_INPUT_PIN_LOW) {  // dah key not pressed, but dit key was pressed
-            buttonPressed = BTN_PRESSED_NONE;
-            key = DIT;
-        }
-        else
-            break;
-    } else {  // dit key was sent
-        if (ditKeyState == GPIO_INPUT_PIN_LOW)  // dit key still pressed
-        {
-            if (buttonPressed == BTN_PRESSED_DAH || dahKeyState == GPIO_INPUT_PIN_LOW)
-            {
-                buttonPressed = BTN_PRESSED_NONE;
-                key = DIT;
-            } else {
-                buttonPressed = BTN_PRESSED_NONE;
-                key = DIT;
-            }
-        } else if (buttonPressed == BTN_PRESSED_DAH || dahKeyState == GPIO_INPUT_PIN_LOW) {  // dit key not pressed, but dah key was pressed
-            buttonPressed = BTN_PRESSED_NONE;
-            key = DAH;
-        }
-        else
-            break;
-    }
-}
-**************************/
